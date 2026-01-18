6d92278ee98ecd8dc701bc9c5f7ad855c214a07a
Bibhu090 <pr.bibhu234@gmail.com>
2026-01-19 01:06:37 +0530

    spring boot app

---
+++ myFirstProj/src/test/java/com/test/myFirstProj/MyFirstProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.myFirstProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
